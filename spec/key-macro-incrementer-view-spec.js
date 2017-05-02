f90cfa9a7db24da8f3f3cf27ffe46ca8ebfe2270
yuji shimada <yuji@yuji-no-MacBook-Pro.local>
2017-05-02 12:53:37 +0900

    first commit.

---
+++ spec/key-macro-incrementer-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import KeyMacroIncrementerView from '../lib/key-macro-incrementer-view';
+
+describe('KeyMacroIncrementerView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
